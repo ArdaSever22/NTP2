23f43bf3ce3297f787d6d928dc6339702f81e1d6
ArdaSever22 <145566153+ArdaSever22@users.noreply.github.com>
2023-09-20 15:57:30 +0300

    Add files via upload

---
+++ MouseHover/MouseHover/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseHover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Leave(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mouse Üzerimde");
+        }
+
+        private void button1_Enter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mouse Üzerimde Değildir");
+        }
+    }
+}
